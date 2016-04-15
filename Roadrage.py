be9ef366a932047f7e5e306ac26a09d40336094a
CameronB <cboudreau6238@gmail.com>
2016-04-15 15:01:07 -0400

    Everything works

--- Roadrage.py
+++ Roadrage.py
@@ -4,10 +4,12 @@ class Road():
 		self.length = length
 		self.curve = curve
 
-	def loop(self , car_list, car):
-		car.position[0] = (car.position[0] - 1000)
-
-		car_list.insert(0,car_list.pop())
+	def road_loop(self , car_list, car, next_car):
+		if next_car.bumper < 6:
+			car.position[0] = 1000
+		else:
+			car.position[0] = (car.position[0] - 1000)
+			car_list.insert(0,car_list.pop())
 		return car_list
 
 class Car():
@@ -19,10 +21,7 @@ class Car():
 		self.speed = speed
 		self.accel = 2
 
-	# def check_car_distance(self, car_list):
-	# 	for car in car_list:
-	# 		if self.position[0] == self.position[1]:
-	# 			pass
+	
 
 	def change_speed(self):
 		# if random.randint(1,10) == 7:
@@ -35,30 +34,18 @@ class Car():
 	def collision_check(self, next_car):
 		difference = next_car.bumper - self.position[0]
 		if difference < self.speed:
-			print("Difference: ", difference)
 			if difference <= 0:
 				self.position[0] = next_car.bumper - 2
 				self.speed = 0
 			else:
 				self.speed = next_car.speed
 
-	# def change_position(self, road):
-	# 	if self.position[0] + self.speed > 1000:
-
-	# 	else:
-	# 		self.position[0] += self.speed
-
 	def move_car(self):
 		self.position[0] += self.speed
 		self.position[1] +=  1
-		# if self.needs_loop():
-		# 	return True
-		# self.change_position()
-		# self.collision_check()
-		# if self.will_slow():
-		# 	self.speed -= 2
 
-	def needs_loop(self):
+
+	def needs_road_loop(self):
 		if self.position[0] > 1000:
 			return True
 
@@ -78,9 +65,10 @@ class Sim():
 				next_car = self.car_list[i+1]
 			else:
 				next_car = self.car_list[0]
+
 			car.move_car()
+			if car.needs_road_loop():
+				road.road_loop(self.car_list, car)
 			car.change_speed()
-			if car.needs_loop():
-				road.loop(self.car_list, car)
 			car.collision_check(next_car)
 		pass
